ffba1578529a9484a2d4e0843c80257c97de68e1
alok1929 <alokhegde221@gmail.com>
2021-02-19 17:29:21 +0530

    rain

--- evenodd.cpp
+++ evenodd.cpp
@@ -2,14 +2,5 @@
 using namespace std;
 int main()
 {
-    cout<<"enter the number";
-    int num;
-    cin>>num;
-    if(num%2==0)
-    {
-        cout<<"even number ";
-    }
-    else{
-        cout<<"odd number";
-    }
+    cout<<"wtf";
 }
