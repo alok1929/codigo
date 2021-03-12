82b2b5eeca1115c7f5466ffa555bfaf2e746e5c8
alokhegde <alokhegde@pop-os.localdomain>
2021-03-13 00:28:49 +0530

    um

--- .vscode/switchSujal.cpp
+++ .vscode/switchSujal.cpp
@@ -10,38 +10,38 @@ int main()
     total=m1+m2+m3+m4;
     float percentage;
     percentage=total/4.0;
-    choice=(int)percentage/10;
+    choice=(int)percentage/10;//choice =6
     cout<<"total marks="<<total<<endl;
     cout<<setprecision(2);
     cout<<"percentage=" << percentage<<endl;
     cout<<"the result of the student is";
-    switch(choice)
-    {
-        case 6:
-        if(choice>=6 && choice <=10)
+        cout<<"choice value"<<choice<<endl;
+        switch(choice)
         {
-            cout<<"first class"<<endl;
+       for(int i=10;i>=6;i--){
+       if(choice==i)
+       {
+           cout<<"first class"<<endl;
+       }
+       }
+       case 5:
+       {
+           cout<<"second class"<<endl;
+       }
+       break;
+       case 4:
+       {
+           cout<<"pass class"<<endl;
+       }
+       break;
+       if(choice<4)
+       {
+           cout<<"stay away from dad "<<endl;
+       }
+
         }
-        break;
-        case 5:
-        if(choice==5)
-        {
-            cout<<"second class"<<endl;
-        }
-        break;
-        case 4:
-        if(choice==4)
-        {
-            cout<<"pass class"<<endl;
-        }
-        break;
-        case 3:
-        if(choice<=3)
-        {
-            cout<<"fail"<<endl;
-        }
-        break;
-        default:cout<<"wrong"<<endl;
-        break;
-    }
+
+   
 }
+
+    
