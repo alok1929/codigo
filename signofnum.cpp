d62fbd740c38301c4e12a9e0fea477f73e071d54
harshith <80906400+harshb55@users.noreply.github.com>
2021-03-27 16:47:19 +0530

    Create signofnum.cpp

---
+++ signofnum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"ENTER THE NUMBER:"<<endl;
+    cin>>a;
+    if(a>=1)
+    cout<<"IT IS POSITIVE NUMBER"<<endl;
+    if(a<=-1)
+    cout<<"IT IS NEGATIVE NUMBER"<<endl;
+}
