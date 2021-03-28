9751cc96f4967d5feec720c9aa6a725017bf8b49
harshith <80906400+harshb55@users.noreply.github.com>
2021-03-28 19:39:40 +0530

    Add files via upload

---
+++ sumofnaturalnum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,n,sum=0;
+    cout<<"ENTER THE NUMBER:";
+    cin>>n;
+    for(a=1;a<=n;++a)
+    sum +=a;
+    cout<<"SUM="<<sum<<endl;
+}
